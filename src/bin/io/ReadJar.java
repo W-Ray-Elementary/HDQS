8e40cee954a067205894f734da2a77f7576675f7
W-Ray <99438@test.com>
2023-08-15 13:56:28 +0800

    补上NewInstance与ReadJar的注释

--- src/bin/io/ReadJar.java
+++ src/bin/io/ReadJar.java
@@ -15,15 +15,31 @@ import java.util.jar.JarFile;
  * 将jar包中的class以对象形式提取出来
  */
 public class ReadJar {
+
+    /**
+     * jar包的文件，应保证不是目录，并且可读，本类会从此文件读取jar包中的class文件，
+     * 并转换为对象。
+     */
     private final File target;
+
+    /**
+     * 读取到jar包中的所有class文件创造的对象
+     */
     private final List<Class<?>> allClass;
 
+    /**
+     * 构造一个ReadJar对象，并在此方法内完成读取class文件并实例化的任务
+     * @param target 在此处的jar包开始搜索
+     */
     public ReadJar(File target) {
         this.target = target;
         allClass = new ArrayList<>();
         initialize();
     }
 
+    /*
+    * 读取class文件并实例化
+    * */
     private void initialize() {
         URL url1;
         try {
@@ -39,7 +55,6 @@ public class ReadJar {
             jarFile = new JarFile(target);
             Enumeration<JarEntry> entries = jarFile.entries();
             List<String> classNames = getClassNames(entries);
-            assert classNames != null;
             for (String x : classNames)
                 loadClass(x, jarClassLoader);
         } catch (IOException e) {
@@ -62,6 +77,9 @@ public class ReadJar {
         }
     }
 
+    /*
+    * 把JarFile所有的JarEntry遍历，是class文件的就返回类名
+    * */
     private List<String> getClassNames(Enumeration<JarEntry> entries) {
         List<String> classNames = new ArrayList<>();
         while (entries.hasMoreElements()) {
@@ -75,6 +93,9 @@ public class ReadJar {
         return classNames;
     }
 
+    /*
+    * 把单个class文件转为对应的class实例，需要ClassLoader
+    * */
     private void loadClass(String x, ClassLoader classLoader) {
         try {
             allClass.add(classLoader.loadClass(x));
--- src/bin/util/NewInstance.java
+++ src/bin/util/NewInstance.java
@@ -4,9 +4,19 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
+/**
+ * 以class对象，创Object示例
+ * （不好意思高考作文写多了）
+ */
 public class NewInstance {
+    /* No NewInstance instance for you */
     private NewInstance() {}
 
+    /**
+     * 相当于{@code new}，但是是个方法
+     * @param clazz 传入的的class对象必须能够实例化
+     * @return 创建的对象会以Object的形式返回
+     */
     public static Object get(Class<?> clazz) {
         Object obj;
         try {
