5141c2ae92bd74de5fadff319f5680bd5b98e95f
W-Ray <99438@test.com>
2023-08-15 09:27:13 +0800

    编写FileSearch的测试

--- src/bin/io/Test.java
+++ src/bin/io/Test.java
@@ -1,6 +1,7 @@
 package bin.io;
 
 import java.io.File;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
@@ -8,11 +9,13 @@ public class Test {
         TEST_PATH();
         TEST_FileToString();
         TEST_ENCODE();
+        TEST_FileSearch();
         long cost = System.currentTimeMillis() - startTime;
         System.out.println("bin.io.FileAndString 的测试成功完成于" + ' ' + cost + ' ' + "ms");
     }
 
     // 此方法用于测试 bin.io.PATH
+
     private static void TEST_PATH() {
         File testFile = PATH.getFile("config.properties");
         String testTxt = testFile.getAbsolutePath();
@@ -20,8 +23,8 @@ public class Test {
             throw new RuntimeException("类 bin.io.PATH 未达成预定目标，测试未完成");
         }
     }
-
     // 此方法用于测试 bin.io.FileToString
+
     private static void TEST_FileToString() {
         // 测试读取
         String testTxt = FileAndString.read(PATH.getFile("testRes\\io\\FileAndString.txt"));
@@ -51,5 +54,12 @@ public class Test {
         FileAndString.write(
                 PATH.getFile("testRes\\io\\编码测试"),
                 "编码测试。\nEncode test.");
+    }
+
+    private static void TEST_FileSearch() {
+        List<File> cfgFiles = FileSearch.suffix(PATH.getFile("testRes"), ".cfg");
+        if (!(cfgFiles.size() > 3) || !(cfgFiles.get(0).getAbsolutePath().endsWith(".cfg"))) {
+            throw new RuntimeException("FileSearch 未能找到文件！");
+        }
     }
 }
