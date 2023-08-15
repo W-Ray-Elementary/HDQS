f0b5509d0fdcf75dea0f0602a06cb8da822e8bd2
W-Ray <99438@test.com>
2023-08-15 21:04:04 +0800

    更新相关脚本

--- 懒人脚本一条龙.bat
+++ 懒人脚本一条龙.bat
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001
 echo.
-echo 编译中
 
 mkdir out
 cd out
@@ -20,7 +19,6 @@ cd ..
 md out\production\HDQS
 chcp 936
 javac -classpath "src;out\production\HDQS\testRes\io\GetCode[77a7c91a].jar" -d ..\out\production\HDQS -encoding UTF-8 src\Launcher.java
-echo 编译完成
 chcp 65001
 echo.
 
