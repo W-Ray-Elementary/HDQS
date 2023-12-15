9f245a40038a8ad9922bc4543d1aab6e7af0807e
W-Ray <99438@test.com>
2023-12-15 19:33:55 +0800

    Operation的参数与初始化

--- src/main/java/com/plzEnterCompanyName/HDQS/text/frame/BT_Operation.java
+++ src/main/java/com/plzEnterCompanyName/HDQS/text/frame/BT_Operation.java
@@ -1,14 +1,40 @@
 package com.plzEnterCompanyName.HDQS.text.frame;
 
 import com.plzEnterCompanyName.HDQS.io.smartIO2.Message;
+import com.plzEnterCompanyName.HDQS.io.smartIO2.Operation;
 import com.plzEnterCompanyName.HDQS.util.Lexicon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BT_Operation extends BlockTypesetter {
+    
+    /**
+     * 要缩进几个字符，该数值会将INDENTATION_CHAR代表的字符重复INDENTATION次后
+     * 加在开头的地方。
+     */
+    protected final int INDENTATION;
+
+    /**
+     * 缩进字符,INDENTATION_CHAR重复INDENTATION次后加在开头的地方。
+     */
+    protected final char INDENTATION_CHAR;
+
+    /**
+     * 单个底部提示之间的间距数
+     */
+    protected final int HORIZONTAL_SPACING;
 
     private int cacheIndex;
 
     protected BT_Operation(SupportedBT_Position position, Lexicon config) {
         super(position);
+        String indentationStr       = config.getFirst("indentation"      );
+        String indentationCharStr   = config.getFirst("indentationChar"  );
+        String horizontalSpacingStr = config.getFirst("horizontalSpacing");
+        INDENTATION        =        Integer.parseInt(indentationStr      );
+        INDENTATION_CHAR   = (char) Integer.parseInt(indentationCharStr  );
+        HORIZONTAL_SPACING =        Integer.parseInt(horizontalSpacingStr);
     }
 
     @Override
